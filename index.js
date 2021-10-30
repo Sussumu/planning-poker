dd904fe7461897d11a057750875d8d5b4d94bab4
Gabriel Sussumu Kato <gabrielsussumu@outlook.com>
2021-10-30 12:14:23 -0300

    Start project with socket.io and client-server event comms

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const httpServer = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(httpServer)
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', socket => {
+    console.log('user connected')
+
+    socket.on('vote', vote => {
+        console.log(`user voted on ${vote}`)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+})
+
+httpServer.listen(3000, () => {
+    console.log('server started at http://localhost:3000')
+})
